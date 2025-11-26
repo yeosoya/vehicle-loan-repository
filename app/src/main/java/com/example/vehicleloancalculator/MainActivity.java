5c2ce1f1c51c87edbca07cab250751aaab6130c7
yeosoya <hanisanajwa27@gmail.com>
2025-11-26 16:40:32 +0800

    add toolbar

--- app/src/main/java/com/example/vehicleloancalculator/MainActivity.java
+++ app/src/main/java/com/example/vehicleloancalculator/MainActivity.java
@@ -25,9 +25,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-            return true;
-        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
 }
