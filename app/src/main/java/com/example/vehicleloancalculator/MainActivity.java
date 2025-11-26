bfb852867f039b8015ccbaba4d21cc980e3e89dc
yeosoya <hanisanajwa27@gmail.com>
2025-11-26 16:34:37 +0800

    main activity java created

--- app/src/main/java/com/example/vehicleloancalculator/MainActivity.java
+++ app/src/main/java/com/example/vehicleloancalculator/MainActivity.java
@@ -16,13 +16,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        //get started button
-        Button btnStart = findViewById(R.id.btnStart);
 
-        btnStart.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, LoanCalculator.class);
-            startActivity(intent);
-        });
     }
 
         @Override
